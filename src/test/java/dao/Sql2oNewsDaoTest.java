27c5f5e8bd9afea9f1f246846543641ce81d38eb
ynyanchoka <monaryymelda@gmail.com>
2022-05-14 17:50:55 +0300

    add userdao tests

--- src/test/java/dao/Sql2oNewsDaoTest.java
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -1,9 +1,13 @@
 package dao;
 
+import interfaces.NewsDao;
+import models.Department;
+import models.News;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -35,6 +39,51 @@ class Sql2oNewsDaoTest {
     public static void shutDown() throws Exception{
         conn.close();
         System.out.println("connection closed");
+    }
+
+    @Test
+    public void addingDepartmentSetsId() throws Exception {
+        News testNews = setupNews();
+        int originalNewsId = testNews.getId();
+        newsDao.add(testNews);
+        assertNotEquals(originalNewsId,testNews.getId());
+    }
+
+    @Test
+    public void getsAllNews() throws Exception{
+        News testNews = setupNews();
+        News otherNews=new News("Training", "Training scheduled tomorrow", "General news");
+        newsDao.add(testNews);
+        newsDao.add(otherNews);
+        assertEquals(2, newsDao.getAllNews().size());
+    }
+
+    @Test
+    public void noNewsReturnsEmpty() throws Exception {
+        assertEquals(0, newsDao.getAllNews().size());
+    }
+    @Test
+    public void deleteByIdDeletesCorrectNews() throws Exception {
+        News testNews = setupNews();
+        newsDao.add(testNews);
+        newsDao.deleteById(testNews.getId());
+        assertEquals(0, newsDao.getAllNews().size());
+    }
+
+    @Test
+    public void clearAll() throws Exception {
+        News testNews = setupNews();
+        News otherNews=new News("Training", "Training scheduled tomorrow", "General news");
+        newsDao.clearAllNews();
+        assertEquals(0, newsDao.getAllNews().size());
+    }
+
+    //HELPER
+    public News setupNews (){
+        return new News ("Training", "Training scheduled tomorrow", "UX", "Department news");
+    }
+    public News setupAltNews (){
+        return new News ("Training", "Training scheduled tomorrow", "General news");
     }
 
 }
